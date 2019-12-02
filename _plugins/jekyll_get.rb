8e1858d326b4d05f2af346c3119ba17b7cabd1cf
Stefan Thoss <stefan.thoss@upstart.com>
2019-12-01 21:51:40 -0800

    Exclude jekyll_get.rb from RuboCop

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -2,33 +2,39 @@ require 'json'
 require 'hash-joiner'
 require 'open-uri'
 
-# https://github.com/18F/jekyll-get
-module JekyllGet
+# See https://github.com/18F/jekyll-get
+module Jekyll_Get
   class Generator < Jekyll::Generator
     safe true
     priority :highest
 
     def generate(site)
       config = site.config['jekyll_get']
-      return unless config
-      config = [config] unless config.is_a?(Array)
+      if !config
+        return
+      end
+      if !config.kind_of?(Array)
+        config = [config]
+      end
       config.each do |d|
-        target = site.data[d['data']]
-        source = JSON.parse(open(d['json']))
-        if target
-          HashJoiner.deep_merge target, source
-        else
-          site.data[d['data']] = source
-        end
-        if d['cache']
-          data_source = (site.config['data_source'] || '_data')
-          path = "#{data_source}/#{d['data']}.json"
-          open(path, 'wb') do |file|
-            file << JSON.generate(site.data[d['data']])
+        begin
+          target = site.data[d['data']]
+          source = JSON.load(open(d['json']))
+          if target
+            HashJoiner.deep_merge target, source
+          else
+            site.data[d['data']] = source
           end
+          if d['cache']
+            data_source = (site.config['data_source'] || '_data')
+            path = "#{data_source}/#{d['data']}.json"
+            open(path, 'wb') do |file|
+              file << JSON.generate(site.data[d['data']])
+            end
+          end
+        rescue
+          next
         end
-      rescue
-        next
       end
     end
   end
